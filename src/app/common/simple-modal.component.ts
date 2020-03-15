b52717695f2d7a7402a0e88714a4df7aa0221188
bkhalifa <benkhalifa.bilel@hotmail.fr>
2020-03-15 23:30:12 +0100

    config input param

--- src/app/common/simple-modal.component.ts
+++ src/app/common/simple-modal.component.ts
@@ -1,9 +1,10 @@
-import { Component, Input } from "@angular/core";
+import { Component, Input, Inject, ViewChild, ElementRef } from "@angular/core";
+import { JQ_TOKEN } from './jquery.service';
 
 @Component({
   selector: 'simple-modal',
   template: `
-  <div id="{{elementId}}" class="modal fade" tabindex="-1">
+  <div id="{{elementId}}"  #modalContainer class="modal fade" tabindex="-1">
     <div class="modal-dialog">
       <div class="modal-content">
         <div class="modal-header">
@@ -12,7 +13,7 @@ import { Component, Input } from "@angular/core";
           data-dismiss="modal"><span> &times;</span> </button>
 
        </div>
-     <div class="modal.body">
+     <div class="modal-body"  (click)="closeModal()">
         <ng-content> </ng-content>
      </div>
   </div>
@@ -25,7 +26,15 @@ import { Component, Input } from "@angular/core";
   `]
 })
 export class SimpleModalComponent {
+  constructor(@Inject(JQ_TOKEN) private $:any){}
   @Input() title: string
   @Input() elementId
-
+  @Input() closeModalConfig:string
+  @ViewChild('modalContainer') modalContainer:ElementRef
+  closeModal(){
+if(this.closeModalConfig ==="true"){
+  this.$(this.modalContainer.nativeElement).modal('hide')
+}
+  //  this.$(this.modalContainer.nativeElement).modal('hide')
+  }
 }
